ae53c8ce16bf94b2cee47e09152a0cbb3806d805
Gustavo Lopez Gianelli <166411314+tronezitos@users.noreply.github.com>
2025-05-24 11:24:05 -0300

    Add files via upload
    Nikocado Avocado Pelado

---
+++ Exercicios 24-05/Exercicio3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args) {
+
+        int NumeroFinal;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.pr
+        intln("Escreva um número");
+        int NumeroTabuada = sc.nextInt();
+        
+        for (int Tabuada = 1; Tabuada <= 10 ; Tabuada++){
+            NumeroFinal = NumeroTabuada * Tabuada;
+            System.out.println(NumeroFinal);
+        } 
+
+        
+    }
+}
